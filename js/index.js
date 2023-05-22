bcbe17f3a757f47933607b82526fa85a846a93cc
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-05-22 00:21:30 -0300

    se agrego musica

--- js/index.js
+++ js/index.js
@@ -1,9 +1,19 @@
 let audio = new Audio('./audio/sientemeAsi.mp3');
 
 let btnPlay = document.getElementById('btn-play');
-let btnPause = document.getElementById('btn-pause');
+let btnPause = document.getElementById('btn-pause')
 
+btnPlay.addEventListener('click', reproducirMusica);
+btnPause.addEventListener('click', pausarMusica);
 
-btnPlay.addEventListener('click', ()=>{
+function reproducirMusica(){
     audio.play();
-})
+    btnPlay.classList.add('d-none');
+    btnPause.classList.remove('d-none');
+}
+
+function pausarMusica(){
+    audio.pause();
+    btnPause.classList.add('d-none');
+    btnPlay.classList.remove('d-none');
+}
